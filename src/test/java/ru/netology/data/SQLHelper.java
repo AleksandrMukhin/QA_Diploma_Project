8eaf66c48c26446c2326c12d2c7de1a98bc7b51e
Aleksandr Mukhin <alexsandrmux@gmail.com>
2023-06-08 21:26:23 +0300

    initial commit

---
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -0,0 +1,73 @@
+package ru.netology.data;
+
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class SQLHelper {
+
+    static QueryRunner runner = new QueryRunner();
+    //static String url = System.getProperty("db.url");
+    //static String url = "jdbc:mysql://localhost:3306/app";
+    static String url = "jdbc:postgresql://localhost:5432/app";
+    static String userName = System.getProperty("db.username");
+    static String password = System.getProperty("db.password");
+
+    public SQLHelper() {
+    }
+
+    @SneakyThrows
+    private static Connection getConn() {
+        return DriverManager.getConnection(url, userName, password);
+    }
+
+
+    public static DataGenerator.CreditData getCardDataWhenBuyingOnCredit() {
+        var cardDataSQL = "SELECT * FROM credit_request_entity ORDER BY created DESC LIMIT 1";
+        try (var conn = getConn()) {
+            var result = runner.query(conn, cardDataSQL,
+                    new BeanHandler<>(DataGenerator.CreditData.class));
+            return result;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public static DataGenerator.DebitCardData getDebitCardData() {
+        var cardDataSQL = "SELECT * FROM payment_entity ORDER BY created DESC LIMIT 1";
+        try (var conn = getConn()) {
+            var result = runner.query(conn, cardDataSQL,
+                    new BeanHandler<>(DataGenerator.DebitCardData.class));
+            return result;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public static DataGenerator.TableOrderEntity getTableOrderEntity() {
+        var orderEntityDataSQL = "SELECT * FROM order_entity ORDER BY created DESC LIMIT 1";
+        try (var conn = getConn()) {
+            var result = runner.query(conn, orderEntityDataSQL,
+                    new BeanHandler<>(DataGenerator.TableOrderEntity.class));
+            return result;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    @SneakyThrows
+    public static void cleanDatabase() {
+        var conn = getConn();
+        runner.execute(conn, "DELETE FROM order_entity");
+        runner.execute(conn, "DELETE FROM payment_entity");
+        runner.execute(conn, "DELETE FROM credit_request_entity");
+    }
+}
